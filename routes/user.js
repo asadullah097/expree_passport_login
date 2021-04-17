7e0d1097c7b2386c2ba8dbb3435a61fbffb7d9af
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2021-04-17 16:40:31 +0500

    intail commit

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/login',(req,res)=>{
+    res.send("Login");
+})
+router.get('/register',(req,res)=>{
+    res.send("Login");
+})
+
+module.exports=router;
